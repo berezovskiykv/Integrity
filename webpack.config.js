7f44a834c99fd2a1da67931171c25401b3d9345d
berezovskiykv <50821233+berezovskiykv@users.noreply.github.com>
2026-04-10 15:25:53 +0700

    Update webpack.config.js
    add real path. Fix error path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './prj.js',
+  entry: './MainScreens/prj.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
